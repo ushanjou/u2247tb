cbbe315066858458662a52db3ba187416d860bc1
anitaloggg <huichen.lo@gmail.com>
2022-04-16 15:18:42 +0800

    main

--- projects/mod04/src/app/app.module.ts
+++ projects/mod04/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
