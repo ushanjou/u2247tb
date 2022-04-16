2c691b8269c3a7954a839210efb58fa281daab66
anitaloggg <huichen.lo@gmail.com>
2022-04-16 13:47:09 +0800

    main

--- projects/mod02/src/app/page1/page1.component.ts
+++ projects/mod02/src/app/page1/page1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Picture } from '../picture';
 
 @Component({
   selector: 'page1',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page1Component implements OnInit {
   Friends=["Jenny", "Vivid", "Kity","Linda"];
-
+  picture = new Picture(1,
+     "https://raw.githubusercontent.com/anitaloggg/git/main/277671026_10159641399788608_4946370506060413057_n.jpg", "大都會公園", "Anita");
+  
   constructor() {
     
    }
---
+++ projects/mod02/src/app/picture.ts
@@ -0,0 +1,8 @@
+export class Picture {
+    constructor(
+        public PictureID: number,
+        public Url: string,
+        public Subject: string,
+        public Author: string) { }
+
+}
