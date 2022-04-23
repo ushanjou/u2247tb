f3990fc7530381d3f0003258ae01d624fe8e7ab1
anitaloggg <huichen.lo@gmail.com>
2022-04-23 14:36:18 +0800

    main

--- projects/mod05/src/app/app.component.ts
+++ projects/mod05/src/app/app.component.ts
@@ -7,18 +7,26 @@ import { CheckStockComponent } from './check-stock/check-stock.component';
   styles: []
 })
 export class AppComponent {
-  @ViewChild("title1") h1: ElementRef|any;
-  @ViewChild(CheckStockComponent) checkStock: CheckStockComponent|any;
+  @ViewChild("title1") h1: ElementRef | any;
+  @ViewChild(CheckStockComponent) checkStock: CheckStockComponent | any;
 
   QTY: number = 12;
   Stock: number = 10;
   title = '購物車';
-  ngAfterViewInit(){
+
+  vDate = new Date();
+  vString = "This is a book";
+  vMoney = 382140;
+  vNumber = 39.85;
+  vNumber2 = 31.11;
+  vPercent = 10;
+
+  ngAfterViewInit() {
     console.log(this.h1.nativeElement);
     console.log("ngAfterViewInit:", this.checkStock);
   }
-  ngAfterViewChecked(){
-    console.log("ngAfterViewChecked: " , this.checkStock);
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked: ", this.checkStock);
   }
 
 }
