4cb8c47ab35ca97d84ceb62a344cd29519ea82d8
anitaloggg <huichen.lo@gmail.com>
2022-04-23 16:14:08 +0800

    main

--- projects/mod06/src/app/page1/page1.component.ts
+++ projects/mod06/src/app/page1/page1.component.ts
@@ -10,7 +10,7 @@ import { MeetingRoom } from '../meeting-room';
 })
 export class Page1Component implements OnInit {
   public capacity = ['5人', '10人', '20人', '30人', '40人']
-  public meetingRoomForm: FormGroup; 
+  public meetingRoomForm: FormGroup;
   meetingRoom: MeetingRoom;
   constructor(public builder: FormBuilder) {
     this.meetingRoom = new MeetingRoom(
@@ -18,13 +18,27 @@ export class Page1Component implements OnInit {
     this.meetingRoomForm = this.builder.group({
       'id': ['', [Validators.required]],
       'name': ['', [Validators.required]],
-      "size": [''], 
-      'projector': [''], 
+      "size": [''],
+      'projector': [''],
       'TV': ['']
     });
   }
   ngOnInit() {
     this.meetingRoomForm.setValue(this.meetingRoom);
   }
-
+  onSubmit() {
+    console.log(this.meetingRoomForm);
+    console.log(this.meetingRoom);
+    // for(let k in this.meetingRoom){
+    //   console.log(k);
+    //   console.log(this.meetingRoom.[k].value);
+    // }
+    this.meetingRoom = {
+      id: this.meetingRoom.id,
+      name: this.meetingRoomForm.get('name')?.value,
+      size: this.meetingRoomForm.get('size')?.value,
+      projector: this.meetingRoomForm.get('projector')?.value,
+      TV: this.meetingRoomForm.get('TV')?.value
+    };
+  }
 }
