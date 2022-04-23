c30128502c4c14b66a0d81352e51998ca7e0bfe3
anitaloggg <huichen.lo@gmail.com>
2022-04-23 14:25:38 +0800

    main

--- projects/mod05/src/app/in-the-box.directive.ts
+++ projects/mod05/src/app/in-the-box.directive.ts
@@ -1,16 +1,27 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[InTheBox]'
 })
 export class InTheBoxDirective {
+  @Input('InTheBox') borderStyle: string = "";
 
-  constructor(private el:ElementRef) {
-this.putInTheBox("solid 2px red");
-   }
+  @HostListener("mouseenter") onMouseEnter() {
+    if (!this.borderStyle)
+      this.borderStyle = "solid 2px red"
+    this.putInTheBox(this.borderStyle);
 
-   private putInTheBox(borderStyle:string){
-    this.el.nativeElement.style.border= borderStyle;
   }
-  
+  @HostListener("mouseleave") onMouseLeave() {
+    this.putInTheBox("none");
+  }
+
+  constructor(private el: ElementRef) {
+    // this.putInTheBox("solid 2px red");
+  }
+
+  private putInTheBox(borderStyle: string) {
+    this.el.nativeElement.style.border = borderStyle;
+  }
+
 }
