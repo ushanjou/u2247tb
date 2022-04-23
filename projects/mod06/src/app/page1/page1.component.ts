0fc6ff2b0cc29c55a2afa6db1e04aa16cbfa34de
anitaloggg <huichen.lo@gmail.com>
2022-04-23 15:47:27 +0800

    main

--- projects/mod06/src/app/page1/page1.component.ts
+++ projects/mod06/src/app/page1/page1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MeetingRoom } from '../meeting-room';
 
 @Component({
   selector: 'app-page1',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class Page1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public capacity = ['5人', '10人', '20人', '30人', '40人']
+  public meetingRoomForm: FormGroup; 
+  meetingRoom: MeetingRoom;
+  constructor(public builder: FormBuilder) {
+    this.meetingRoom = new MeetingRoom(
+      101, '哥多華', this.capacity[2], false, true);
+    this.meetingRoomForm = this.builder.group({
+      'id': ['', [Validators.required]],
+      'name': ['', [Validators.required]],
+      "size": [''], 
+      'projector': [''], 
+      'TV': ['']
+    });
+  }
+  ngOnInit() {
+    this.meetingRoomForm.setValue(this.meetingRoom);
   }
 
 }
