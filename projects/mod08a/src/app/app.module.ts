827d9377d5b9dd5854197afa3d77d271b2acc4b1
anitaloggg <huichen.lo@gmail.com>
2022-05-07 13:18:43 +0800

    main

---
+++ projects/mod08a/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page1Component,
+    Page2Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
