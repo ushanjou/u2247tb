2f867676a10d8f1909a8179d826b650377f3b3d0
anitaloggg <huichen.lo@gmail.com>
2022-05-21 13:35:20 +0800

    main

--- projects/mod14/src/app/auth/user-name/user-name.component.ts
+++ projects/mod14/src/app/auth/user-name/user-name.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-user-name',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserNameComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
